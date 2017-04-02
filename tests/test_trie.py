0643b013520d5a576593fc093a7da8c3376fa917
beepscore <support@beepscore.com>
2017-04-02 16:27:38 -0700

    Delete obsolete comment

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -8,7 +8,6 @@ from trie_me import node
 class TestTrie(unittest.TestCase):
 
     def test_instantiate(self):
-        # numbers_filename = "data/input/numbers_test.txt"
         root_node = None
         numbers_trie = trie.Trie(root_node)
         self.assertIsNotNone(numbers_trie)
