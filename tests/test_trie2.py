0ab3e00b78b585cb5370d4076af28041862bb7cc
beepscore <support@beepscore.com>
2017-04-04 21:54:08 -0700

    In test_trie2 delete some commented out and unused code.

--- tests/test_trie2.py
+++ tests/test_trie2.py
@@ -9,10 +9,7 @@ class TestTrie(unittest.TestCase):
     def test_trie2(self):
 
         trie = trie2.Trie2.make_trie('hello', 'abc', 'baz', 'bar', 'barz')
-        # print(trie)
-        # get_file()
-        words = []
-        # list(trie, "", words)
+        
         print(trie2.Trie2.in_trie(trie, 'bar'))
         print(trie2.Trie2.in_trie(trie, 'bab'))
         print(trie2.Trie2.in_trie(trie, 'zzz'))
