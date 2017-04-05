c75c03098995beb7b5ec508e18900d2ba10a276b
beepscore <support@beepscore.com>
2017-04-04 22:34:08 -0700

    Extract test_in_trie

--- tests/test_trie2.py
+++ tests/test_trie2.py
@@ -6,7 +6,7 @@ from trie_me import trie2
 
 class TestTrie2(unittest.TestCase):
 
-    def test_trie2(self):
+    def test_in_trie(self):
 
         trie = trie2.Trie2.make_trie('hello', 'abc', 'baz', 'bar', 'barz')
 
@@ -18,9 +18,12 @@ class TestTrie2(unittest.TestCase):
         trie2.Trie2.add(trie, "bax")
         self.assertTrue(trie2.Trie2.in_trie(trie, 'bax'))
 
-        self.assertTrue(trie2.Trie2.in_trie(trie, 'baz'))
+    def test_trie2(self):
+
+        trie = trie2.Trie2.make_trie('hello', 'abc', 'baz', 'bar', 'barz')
+        trie2.Trie2.add(trie, "bax")
 
         self.assertEqual(trie, {'h': {'e': {'l': {'l': {'o': {'_': '_'}}}}}, 'a': {'b': {'c': {'_': '_'}}},
-         'b': {'a': {'z': {'_': '_'}, 'r': {'_': '_', 'z': {'_': '_'}}, 'x': {'_': '_'}}}})
+                                'b': {'a': {'z': {'_': '_'}, 'r': {'_': '_', 'z': {'_': '_'}}, 'x': {'_': '_'}}}})
 
         self.assertEqual(trie2.Trie2.list_words(trie), ['hello', 'abc', 'baz', 'bar', 'barz', 'bax'])
