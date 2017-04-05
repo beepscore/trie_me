a49c41e3fe2d113a47cb1ae2159aba739b89c5d7
beepscore <support@beepscore.com>
2017-04-04 22:30:23 -0700

    In test_trie2 change class to TestTrie2 and change print statements to test assertions. These tests pass.

--- tests/test_trie2.py
+++ tests/test_trie2.py
@@ -4,17 +4,22 @@ import unittest
 from trie_me import trie2
 
 
-class TestTrie(unittest.TestCase):
+class TestTrie2(unittest.TestCase):
 
     def test_trie2(self):
 
         trie = trie2.Trie2.make_trie('hello', 'abc', 'baz', 'bar', 'barz')
 
-        print(trie2.Trie2.in_trie(trie, 'bar'))
-        print(trie2.Trie2.in_trie(trie, 'bab'))
-        print(trie2.Trie2.in_trie(trie, 'zzz'))
+        self.assertTrue(trie2.Trie2.in_trie(trie, 'bar'))
+        self.assertFalse(trie2.Trie2.in_trie(trie, 'bab'))
+        self.assertFalse(trie2.Trie2.in_trie(trie, 'zzz'))
+
         trie2.Trie2.add(trie, "bax")
-        print(trie2.Trie2.in_trie(trie, 'bax'))
-        print(trie2.Trie2.in_trie(trie, 'baz'))
-        print(trie)
-        print(trie2.Trie2.list_words(trie))
+        self.assertTrue(trie2.Trie2.in_trie(trie, 'bax'))
+        
+        self.assertTrue(trie2.Trie2.in_trie(trie, 'baz'))
+
+        self.assertEqual(trie, {'h': {'e': {'l': {'l': {'o': {'_': '_'}}}}}, 'a': {'b': {'c': {'_': '_'}}},
+         'b': {'a': {'z': {'_': '_'}, 'r': {'_': '_', 'z': {'_': '_'}}, 'x': {'_': '_'}}}})
+
+        self.assertEqual(trie2.Trie2.list_words(trie), ['hello', 'abc', 'baz', 'bar', 'barz', 'bax'])
