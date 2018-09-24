f7e44a0ecc6c9e839add33e3ad5c7746dbc4290c
beepscore <support@beepscore.com>
2018-09-24 16:14:36 -0700

    In node change name to an attribute (instance variable) and always initialize it to None.

--- node.py
+++ node.py
@@ -10,8 +10,6 @@ class Node:
     # ignore root node
     keys = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
-    name = None
-    """str: e.g. In a trie of social security numbers, leaf node name could be the person's name."""
 
     # @staticmethod is independent of instance state
     # https://www.youtube.com/watch?v=HTLu2DFOdTg
@@ -31,11 +29,12 @@ class Node:
         this simplifies implementation
         https://www.toptal.com/java/the-trie-a-neglected-data-structure
         TODO: Consider if this is memory inefficient and if it is worth optimizing
-
         :return: None
         """
         # http://stackoverflow.com/questions/10712002/create-an-empty-list-in-python-with-certain-size#10712044
         self.children = [None, None, None, None, None, None, None, None, None, None]
+        # In a trie of social security numbers, leaf node name could be the person's name.
+        self.name = None
 
     def is_leaf_node(self) -> bool:
         """
